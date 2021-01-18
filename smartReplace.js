737d89b1e2eeceb02fb32968500cb7503cf21006
jacky <jackpater666@gmail.com>
2021-01-18 21:27:17 +0800

    jackpater/JD_Actions_Code

--- smartReplace.js
+++ smartReplace.js
@@ -163,75 +163,75 @@ function batchReplace() {
 
 async function downloader_jd() {
     if (/require\(['"`]{1}.\/jdCookie.js['"`]{1}\)/.test(remoteContent))
-        await download("https://github.com/jackpater/JD_Actions_Code/raw/dj_code/jdCookie.js", "./jdCookie.js", "京东Cookies");
+        await download("https://github.com/jackpater/JD_Actions_Code/raw/jdCookie.js", "./jdCookie.js", "京东Cookies");
     if (remoteContent.indexOf("jdFruitShareCodes") > 0) {
         await download(
-            "https://github.com/jackpater/JD_Actions_Code/raw/dj_code/jdFruitShareCodes.js",
+            "https://github.com/jackpater/JD_Actions_Code/raw/jdFruitShareCodes.js",
             "./jdFruitShareCodes.js",
             "东东农场互助码"
         );
     }
     if (remoteContent.indexOf("jdPetShareCodes") > 0) {
         await download(
-            "https://github.com/jackpater/JD_Actions_Code/raw/dj_code/jdPetShareCodes.js",
+            "https://github.com/jackpater/JD_Actions_Code/raw/jdPetShareCodes.js",
             "./jdPetShareCodes.js",
             "京东萌宠"
         );
     }
     if (remoteContent.indexOf("jdPlantBeanShareCodes") > 0) {
         await download(
-            "https://github.com/jackpater/JD_Actions_Code/raw/dj_code/jdPlantBeanShareCodes.js",
+            "https://github.com/jackpater/JD_Actions_Code/raw/jdPlantBeanShareCodes.js",
             "./jdPlantBeanShareCodes.js",
             "种豆得豆互助码"
         );
     }
     if (remoteContent.indexOf("jdSuperMarketShareCodes") > 0)
         await download(
-            "https://github.com/jackpater/JD_Actions_Code/raw/dj_code/jdSuperMarketShareCodes.js",
+            "https://github.com/jackpater/JD_Actions_Code/raw/jdSuperMarketShareCodes.js",
             "./jdSuperMarketShareCodes.js",
             "京小超互助码"
         );
     if (remoteContent.indexOf("jdFactoryShareCodes") > 0) {
         await download(
-            "https://github.com/jackpater/JD_Actions_Code/raw/dj_code/jdFactoryShareCodes.js",
+            "https://github.com/jackpater/JD_Actions_Code/raw/jdFactoryShareCodes.js",
             "./jdFactoryShareCodes.js",
             "东东工厂互助码"
         );
     }
     if (remoteContent.indexOf("jdDreamFactoryShareCodes") > 0) {
         await download(
-            "https://github.com/jackpater/JD_Actions_Code/raw/dj_code/jdDreamFactoryShareCodes.js",
+            "https://github.com/jackpater/JD_Actions_Code/raw/jdDreamFactoryShareCodes.js",
             "./jdDreamFactoryShareCodes.js",
             "京喜工厂互助码"
         );
     }
     if (remoteContent.indexOf("new Env('京喜农场')") > 0) {
         await download(
-            "https://github.com/jackpater/JD_Actions_Code/raw/dj_code/jdJxncTokens.js",
+            "https://github.com/jackpater/JD_Actions_Code/raw/jdJxncTokens.js",
             "./jdJxncTokens.js",
             "京喜农场Token"
         );
         await download(
-            "https://github.com/jackpater/JD_Actions_Code/raw/dj_code/jdJxncShareCodes.js",
+            "https://github.com/jackpater/JD_Actions_Code/raw/jdJxncShareCodes.js",
             "./jdJxncShareCodes.js",
             "京喜农场分享码"
         );
         await download(
-            "https://github.com/jackpater/JD_Actions_Code/raw/dj_code/USER_AGENTS.js",
+            "https://github.com/jackpater/JD_Actions_Code/raw/USER_AGENTS.js",
             "./USER_AGENTS.js",
             "USER_AGENTS"
         );
     }
     if (remoteContent.indexOf("jdDreamFactoryShareCodes") > 0) {
         await download(
-            "https://github.com/jackpater/JD_Actions_Code/raw/dj_code/jdDreamFactoryShareCodes.js",
+            "https://github.com/jackpater/JD_Actions_Code/raw/jdDreamFactoryShareCodes.js",
             "./jdDreamFactoryShareCodes.js",
             "京喜工厂互助码"
         );
     }
     if (remoteContent.indexOf("new Env('京喜财富岛')") > 0) {
         await download(
-            "https://github.com/jackpater/JD_Actions_Code/raw/dj_code/jdJxncTokens.js",
+            "https://github.com/jackpater/JD_Actions_Code/raw/jdJxncTokens.js",
             "./jdJxncTokens.js",
             "京喜农场Token"
         );
@@ -239,11 +239,11 @@ async function downloader_jd() {
 }
 
 async function downloader_notify() {
-    await download("https://github.com/jackpater/JD_Actions_Code/raw/dj_code/sendNotify.js", "./sendNotify.js", "统一通知");
+    await download("https://github.com/jackpater/JD_Actions_Code/raw/sendNotify.js", "./sendNotify.js", "统一通知");
 }
 
 async function downloader_user_agents() {
-    await download("https://github.com/jackpater/JD_Actions_Code/raw/dj_code/USER_AGENTS.js", "./USER_AGENTS.js", "云端UA");
+    await download("https://github.com/jackpater/JD_Actions_Code/raw/USER_AGENTS.js", "./USER_AGENTS.js", "云端UA");
 }
 
 async function download(url, path, target) {
